e12b27211420231a9e329288c0b3c352c2c134d8
Taymaz Ranjbari <89608125+TaymazRAN@users.noreply.github.com>
2023-09-04 14:23:00 +0330

    add

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import PostForm from "./components/PostForm";
+import PostList from "./components/PostList";
+
+function App() {
+  return (
+    <>
+      <PostForm />
+      <PostList />
+    </>
+  );
+}
+
+export default App;
